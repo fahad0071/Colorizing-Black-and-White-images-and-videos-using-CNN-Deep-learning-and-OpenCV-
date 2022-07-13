cc67f57ff59ef398db5f1a385f122ef83a9434b8
fahad0071 <105378742+fahad0071@users.noreply.github.com>
2022-07-13 18:55:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from turtle import clear, width
+from Colorizer import *
+
+colorizer = Colorizer(width = 640, height = 480)
+
+colorizer.processImage("input/6.jpeg")
+
+colorizer.processVideo("input/girl2.mp4")
